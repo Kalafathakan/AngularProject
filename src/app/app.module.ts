664762a72190238bf836f9b58a4e52d3a4182c26
Oreoluwatemi <temitayolawal35@gmail.com>
2022-07-13 14:54:38 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContactComponent } from './contact/contact.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    FooterComponent,
+    ContactComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule,
