699ceffd719794e2c1b2de29208a69162a7662e0
Nicholas Keller-Sedan <104579750+NKellerSedan@users.noreply.github.com>
2022-07-16 17:49:17 -0400

    Updated Gallery

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -9,6 +9,12 @@ export class GalleryComponent implements OnInit {
 
   constructor() { }
 
+  chefs = 'These are our chefs that prepare our delicious menu!';
+  kitchen = 'A view of our spacious kitchen our chefs use to prepare your meal!';
+  patio = 'Ask about dining on our beautiful outdoor patio!';
+  customerService = 'Our staff are always delivering excellent service to all tables directly!';
+
+
   ngOnInit(): void {
   }
 
