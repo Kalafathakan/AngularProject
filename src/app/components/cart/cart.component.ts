88bb174f98fb47e6ef7345f4e3bedea944c87a5c
Vishkujo <vepicage@gmail.com>
2022-08-07 00:52:34 -0400

    Done with cart

--- src/app/components/cart/cart.component.ts
+++ src/app/components/cart/cart.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CartService } from 'src/app/Services/cart.service';
+import { MenuType } from 'src/app/Services/menu.service';
+import { OrderService } from 'src/app/Services/order.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cart',
@@ -8,9 +12,23 @@ import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 })
 export class CartComponent implements OnInit {
 
-  constructor() { }
+  public items: Array<MenuType> = [];
+  public totalPrice: number = 0;
+  public cartList: string = "";
 
-  ngOnInit(): void {
+  constructor(private cartService: CartService, private orderService: OrderService, private router: Router) { }
+
+  ngOnInit() {
+    this.cartService.currentDataCart$.subscribe(x => {
+      if (x) {
+        this.items = x;
+        this.totalPrice = x.reduce((sum, current) => sum + (current.price * current.quantity), 0);
+      }
+    })
+  }
+
+  public remove(food: MenuType) {
+    this.cartService.removeCart(food);
   }
 
   CheckoutForm = new FormGroup({
@@ -20,7 +38,29 @@ export class CartComponent implements OnInit {
 
   onSubmit() {
     console.log(this.CheckoutForm);
-    //this.myReviewService.addReview(ngForm).subscribe((data) => console.log(data));
+
+    // Create cart list
+    this.items.forEach((food) => {
+      this.cartList += food.food_name + ', ';
+    });
+
+    if (this.cartList.slice(-2) === ', ') {
+      this.cartList = this.cartList.substring(0, this.cartList.length - 2);
+    }
+
+    // POST to orders table
+    this.orderService.addOrder(
+      this.CheckoutForm.value.name!,
+      this.CheckoutForm.value.phone!,
+      this.cartList!,
+      this.totalPrice.toFixed(2).toString()!
+    ).subscribe();
+
+    // Go to orders page after a short delay so database can update
+    setTimeout(() => {
+      this.router.navigateByUrl("/orders");
+    }
+      , 1000);
   }
 
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminServiceService } from 'src/app/Services/admin-service.service';
-import { MenuService } from 'src/app/Services/menu.service';
+import { MenuService, MenuType } from 'src/app/Services/menu.service';
+import { CartService } from 'src/app/Services/cart.service';
 
 @Component({
   selector: 'app-menu',
@@ -10,13 +11,20 @@ import { MenuService } from 'src/app/Services/menu.service';
 export class MenuComponent implements OnInit {
 
   searchValue: string = '';
+  totalQuantity: number = 0;
   categories: string[] = ["Starters", "Mains", "Curries", "Desserts", "Beverages"];
-  constructor( private myMenuService: MenuService, public adminService: AdminServiceService) { }
+  constructor( private myMenuService: MenuService, public adminService: AdminServiceService, private cartService:CartService) { }
 
   ngOnInit(): void {
     this.myMenuService.getMenu().subscribe((data) => {
      this.adminService.setMenu(data) 
     });
+  }
+
+  public addCart(food:MenuType)
+  {
+    this.cartService.addCart(food);
+    this.totalQuantity++;
   }
 
 }
